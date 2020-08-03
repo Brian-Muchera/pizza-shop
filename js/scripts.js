34f93dcdca6a1849605214954e8e903a52ca3edc
Brian-Muchera <mucherabrian2@gmail.com>
2020-08-03 13:27:48 +0300

    made changes

--- js/scripts.js
+++ js/scripts.js
@@ -24,28 +24,28 @@ order.prototype.cost = function() {
     }
 
     if (this.crust === pizzaCrust[0]) {
-        this.price += 100;
+        this.price += 200;
     } else if (this.crust === pizzaCrust[1]) {
-        this.price += 100;
+        this.price += 200;
     } else if (this.crust === pizzaCrust[2]) {
-        this.price += 50;
+        this.price += 100;
     }
 
     if (this.nonVegTopping === pizzaNonVegTopping[0]) {
-        this.price += 300;
+        this.price += 500;
     } else if (this.nonVegTopping === pizzaNonVegTopping[1]) {
-        this.price += 300;
+        this.price += 400;
     } else if (this.nonVegTopping === pizzaNonVegTopping[2]) {
-        this.price += 300;
+        this.price += 600;
     }
 
 
     if (this.vegTopping === pizzaVegTopping[0]) {
-        this.price += 200;
+        this.price += 400;
     } else if (this.vegTopping === pizzaVegTopping[1]) {
-        this.price += 200;
+        this.price += 500;
     } else if (this.vegTopping === pizzaVegTopping[2]) {
-        this.price += 200;
+        this.price += 600;
     }
 
     return this.price;
