50b7ff430612993934588d77fa2a8a3312f4662e
Brian-Muchera <mucherabrian2@gmail.com>
2020-08-03 10:34:28 +0300

    modified scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//user interface
+var totalCosts = [];
+
+function order(size, crust, vegTopping, nonVegTopping) {
+    this.size = size;
+    this.crust = crust;
+    this.nonVegTopping = nonVegTopping;
+    this.vegTopping = vegTopping;
+    this.price = 0;
+
+}
