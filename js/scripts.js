a7d74fbbba38dc0d20d921d5835fd899c422bdb5
Brian-Muchera <mucherabrian2@gmail.com>
2020-08-03 10:47:51 +0300

    modified scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,8 @@ function order(size, crust, vegTopping, nonVegTopping) {
     this.price = 0;
 
 }
+var pizzaSize = ["Small", "Medium", "Large"];
+var pizzaCrust = ["Crispy", "Stuffed", "Gluten-free"];
+var pizzaNonVegTopping = ["Chicken", "Sausage", "Bacon", ];
+var pizzaVegTopping = ["Mushrooms", "Onions", "Capsicum", ];
+
