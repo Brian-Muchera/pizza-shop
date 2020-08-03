498519b61da06ff028f18641b9127b6cad7fbbd2
Brian-Muchera <mucherabrian2@gmail.com>
2020-08-03 10:50:44 +0300

    modified scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,18 @@ var pizzaCrust = ["Crispy", "Stuffed", "Gluten-free"];
 var pizzaNonVegTopping = ["Chicken", "Sausage", "Bacon", ];
 var pizzaVegTopping = ["Mushrooms", "Onions", "Capsicum", ];
 
+order.prototype.cost = function() {
+    if (this.size === pizzaSize[0]) {
+        this.price += 500;
+    } else if (this.size === pizzaSize[1]) {
+        this.price += 700;
+    } else if (this.size === pizzaSize[2]) {
+        this.price += 900;
+    }
+    if (this.crust === pizzaCrust[0]) {
+        this.price += 200;
+    } else if (this.crust === pizzaCrust[1]) {
+        this.price += 200;
+    } else if (this.crust === pizzaCrust[2]) {
+        this.price += 100;
+    }
